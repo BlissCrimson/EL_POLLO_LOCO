e350bcd4c46f5c4da01afbe4d3be08dafae84023
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-05-28 14:59:14 +0200

    feat: add images ti character.class.js and add function to animate

--- models/character.class.js
+++ models/character.class.js
@@ -3,9 +3,57 @@ class Character extends MovableObject {
     y = 280;
     width = 112;
     height = 220;
+    IMAGES_WALKING = [
+        'assets/img/2_character_pepe/2_walk/W-21.png',
+        'assets/img/2_character_pepe/2_walk/W-22.png',
+        'assets/img/2_character_pepe/2_walk/W-23.png',
+        'assets/img/2_character_pepe/2_walk/W-24.png',
+        'assets/img/2_character_pepe/2_walk/W-25.png',
+        'assets/img/2_character_pepe/2_walk/W-26.png'
+    ];
+    IMAGES_JUMP = [
+        'assets/img/2_character_pepe/3_jump/J-31.png',
+        'assets/img/2_character_pepe/3_jump/J-32.png',
+        'assets/img/2_character_pepe/3_jump/J-33.png',
+        'assets/img/2_character_pepe/3_jump/J-34.png',
+        'assets/img/2_character_pepe/3_jump/J-35.png',
+        'assets/img/2_character_pepe/3_jump/J-36.png',
+        'assets/img/2_character_pepe/3_jump/J-37.png',
+        'assets/img/2_character_pepe/3_jump/J-38.png',
+        'assets/img/2_character_pepe/3_jump/J-39.png'
+    ];
+    IMAGES_HURT = [
+        'assets/img/2_character_pepe/4_hurt/H-41.png',
+        'assets/img/2_character_pepe/4_hurt/H-42.png',
+        'assets/img/2_character_pepe/4_hurt/H-43.png'
+    ];
+    IMAGES_DEAD = [
+        'assets/img/2_character_pepe/5_dead/D-51.png',
+        'assets/img/2_character_pepe/5_dead/D-52.png',
+        'assets/img/2_character_pepe/5_dead/D-53.png',
+        'assets/img/2_character_pepe/5_dead/D-54.png',
+        'assets/img/2_character_pepe/5_dead/D-55.png',
+        'assets/img/2_character_pepe/5_dead/D-56.png',
+        'assets/img/2_character_pepe/5_dead/D-57.png'
+    ];
+    currentImage = 0;
+
     constructor() {
-        super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png')
+        super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
         this.y = 430 - this.height
+        this.animate();
+    }
+
+    animate() {
+
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1, Rest 1
+            // i = 0, 1, 2, 3, 4, 5, 0
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 6000 / 60);
     }
 
     jump() {
