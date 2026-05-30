4a122415c70f9e17886d673fa9ee7dcbae123bea
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-05-30 23:24:57 +0200

    feat: add walk animation and walk and reverse walk funktion

--- models/character.class.js
+++ models/character.class.js
@@ -1,8 +1,9 @@
 class Character extends MovableObject {
-    x = 24;
+    x = 64;
     y = 280;
     width = 112;
     height = 220;
+    speed = 2;
     IMAGES_WALKING = [
         'assets/img/2_character_pepe/2_walk/W-21.png',
         'assets/img/2_character_pepe/2_walk/W-22.png',
@@ -50,6 +51,17 @@ class Character extends MovableObject {
 
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.D) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT || this.world.keyboard.A) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.D || this.world.keyboard.LEFT || this.world.keyboard.A) {
+
+                // Walk Animation
                 let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 7 % 6; => 1, Rest 1
                 // i = 0, 1, 2, 3, 4, 5, 0
                 let path = this.IMAGES_WALKING[i];
