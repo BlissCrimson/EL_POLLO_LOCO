ebec1429c9aa4cac5b3ac1ec7a43c895f5898bd1
Matthias Tausch <31157558+BlissCrimson@users.noreply.github.com>
2026-06-24 12:18:32 +0200

    feat: add idle images for character pepe

--- models/character.class.js
+++ models/character.class.js
@@ -38,6 +38,30 @@ class Character extends MovableObject {
         'assets/img/2_character_pepe/5_dead/D-56.png',
         'assets/img/2_character_pepe/5_dead/D-57.png'
     ];
+    IMAGES_IDLE = [
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-1.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-2.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-3.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-4.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-5.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-6.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-7.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-8.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-9.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/idle/I-10.png'
+    ]
+    IMAGES_LONG_IDLE = [
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-11.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-12.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-13.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-14.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-15.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-16.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-17.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-18.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-19.png',
+        'docs/img_pollo_locco/2_character_pepe/1_idle/long_idle/I-20.png'
+    ]
     currentImage = 0;
     world;
 
